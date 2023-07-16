838df289e8ba0d5cc5df1a305c0655debeee6281
billhamilton68 <billhamilton68@gmail.com>
2023-07-16 12:11:45 -0700

    updates

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -39,6 +39,42 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/posts/:id/comments', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ['username']
+            }
+          ]
+        }
+      ],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    const post = postData.get({ plain: true });
+
+    res.render('viewComments', {
+      post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/contact', (req, res) => {
   res.render('contact', {
     logged_in: req.session.logged_in,
